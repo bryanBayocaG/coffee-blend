7716584e029c570412a498e2868d2065879d138d
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-06-29 15:56:04 +0800

    register backend

--- auth/register.php
+++ auth/register.php
@@ -1,4 +1,25 @@
 <?php require "../includes/header.php"; ?>
+<?php require "../config/config.php"; ?>
+<?php 
+ if(isset($_POST['submit'])){
+    if(empty($_POST['username']) OR empty($_POST['email']) OR empty($_POST['password'])) {
+      echo "<script>alert('One or more inputs are empty')</script>;";
+    } else {
+      $username = $_POST['username'];
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT) ;
+      $email = $_POST['email'];
+
+      $insert = $conn->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
+      $insert->execute([
+        ":username" => $username,
+        ":email" => $email,
+        ":password" => $password,
+      ]);
+
+      header("location: login.php");
+    }
+ }
+?>
 
 <section class="home-slider owl-carousel">
     <div class="slider-item" style="background-image: url(<?php echo APPURL?>/images/bg_2.jpg);"
@@ -22,38 +43,36 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 ftco-animate">
-                <form action="#" class="billing-form ftco-bg-dark p-3 p-md-5">
+                <form action="register.php" method="POST" class="billing-form ftco-bg-dark p-3 p-md-5">
                     <h3 class="mb-4 billing-heading">Register</h3>
                     <div class="row align-items-end">
                         <div class="col-md-12">
                             <div class="form-group">
                                 <label for="Username">Username</label>
-                                <input type="text" class="form-control" placeholder="Username">
+                                <input type="text" class="form-control" name="username" placeholder="Username">
                             </div>
                         </div>
                         <div class="col-md-12">
                             <div class="form-group">
                                 <label for="Email">Email</label>
-                                <input type="text" class="form-control" placeholder="Email">
+                                <input type="text" class="form-control" name="email" placeholder="Email">
                             </div>
                         </div>
 
                         <div class="col-md-12">
                             <div class="form-group">
                                 <label for="Password">Password</label>
-                                <input type="password" class="form-control" placeholder="Password">
+                                <input type="password" class="form-control" name="password" placeholder="Password">
                             </div>
 
                         </div>
                         <div class="col-md-12">
                             <div class="form-group mt-4">
                                 <div class="radio">
-                                    <button class="btn btn-primary py-3 px-4">Register</button>
+                                    <button name="submit" class="btn btn-primary py-3 px-4">Register</button>
                                 </div>
                             </div>
                         </div>
-
-
                 </form><!-- END -->
             </div> <!-- .col-md-8 -->
         </div>
