b02f0cc7759a14a77d5aa339d918a77b5ee46870
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-06-29 15:17:09 +0800

    register page

--- config/config.php
+++ config/config.php
@@ -1,7 +1,13 @@
 <?php
+try{
     define("HOST", "localhost");
     define("DBNAME", "coffee-blend");
     define("USER", "root");
     define("PASS", "");
 
     $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASS);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $Exception) {
+    echo $Exception->getMessage();
+}
